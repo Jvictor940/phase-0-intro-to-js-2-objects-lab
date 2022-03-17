e622c0ea58c566b43375fa3b75479925e55a18a1
Jerry <Jvictor940@gmail.com>
2022-03-17 10:18:03 -0400

    commit tree c4a01e129e9742463861609032859ea39fdf8ded

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution in this file!
+const employee = {
+    name: "Jerry",
+    streetAddress: "11 Broadway",
+
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const updatedEmployee = [...employee, "Jack"]
+}
